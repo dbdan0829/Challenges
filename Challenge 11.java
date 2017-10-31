f47d6937177942f06aa3595194b933fce8831f5f
dbdan0829 <33257647+dbdan0829@users.noreply.github.com>
2017-10-31 14:37:57 +0000

    Create Challenge 11.java

---
+++ Challenge 11.java
@@ -0,0 +1,19 @@
+package challenges;
+import java.util.Scanner;
+public class Challenges {
+
+    public static void main(String[] args) {
+        
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = reader.nextInt();
+        
+        for (int i = 0; i <= n; i++) {
+            // contents
+            System.out.println(i);
+                
+        } 
+        
+    }
+    
+}
