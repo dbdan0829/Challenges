bf2faab305ae64ead76b4b9e9cdf4cbf3081f386
dbdan0829 <33257647+dbdan0829@users.noreply.github.com>
2017-10-31 14:15:21 +0000

    Create Challenge 2.java

---
+++ Challenge 2.java
@@ -0,0 +1,14 @@
+package challenges;
+public class Challenges {
+
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 121; i = i + 10) {
+            // contents
+            System.out.println(i);
+                
+        } 
+        
+    }
+    
+}
